7371bafc307910aa5b4277dd9dc361b63e01bb55
Komputer Sala20 <user@ubuntu-sala20.(none)>
2011-11-18 11:36:36 +0100

    added plexus-server

---
+++ plexus-server/src/main/java/pl/marines-project/plexus-server/App.java
@@ -0,0 +1,13 @@
+package pl.marines-project.plexus-server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
